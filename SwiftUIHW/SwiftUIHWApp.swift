e73a098c8a1e705c01dcfdc777ef1d0936d9a5bf
vipersona666 <cross.84@mail.ru>
2023-09-13 11:18:47 +0300

    Initial Commit

---
+++ SwiftUIHW/SwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHWApp.swift
+//  SwiftUIHW
+//
+//  Created by Andrei on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
